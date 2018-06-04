da95f805f99273ac91d58df214c282ecfc6e00de
joe lee <as955133@gmail.com>
2018-06-04 20:37:55 +0800

    加入專案檔案。

---
+++ ConsoleApp_存取元素/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_存取元素
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
